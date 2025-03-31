a395af17aa1b3b63d937f5c1b8de6bda20c639c0
Clara Rocha Ferreira <clararf.ferreira@gmail.com>
2025-03-31 15:27:15 -0300

    Conectando a rota criada com a API criada e alterando o banco de dados

--- controle-serie/routes/web.php
+++ controle-serie/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Api\SeriesController;
+use App\Models\Serie;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/api/series', function () {
-    return [];
-});
+Route::get('/api/series', [\App\Http\Controllers\Api\SeriesController::class, 'index']);
