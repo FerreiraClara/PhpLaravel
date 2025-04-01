650ab30650192169481b3e6380b510eff138f6b5
Clara Rocha Ferreira <clararf.ferreira@gmail.com>
2025-04-01 16:32:34 -0300

    Criando Models e Migracoes - Episode e Season | Criaçndo Repositoriosçççõçõ

--- controle-serie/app/Http/Controllers/Api/SeriesController.php
+++ controle-serie/app/Http/Controllers/Api/SeriesController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Serie;
 use App\Http\Requests\SeriesFormRequest;
+use App\Repositories\SeriesRepository;
+use App\Repositories\EloquentSeriesRepository;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+
+    }
+
     public function index()
     {
         return Serie::all();
@@ -15,11 +22,8 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        // verificando se os dados estão sendo recebidos com sucesso
-        $serie = Serie::create($request->all());
-
         //informando ao usuário que foi criado e retornando-a
         return response() 
-            ->json($serie, 201);
+            ->json($this->seriesRepository->add($request), 201);
     }
 }
