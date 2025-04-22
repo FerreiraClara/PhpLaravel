fc4ceee3f16fe96c40c740af4741b748c8cd7efc
Clara Rocha Ferreira <clararf.ferreira@gmail.com>
2025-04-22 12:57:52 -0300

    Adicionando paginas linkadas do navbar - Projeto LaravelFilamentBeginnerPraticalCourse

--- LaravelFilamentBeginnerPraticalCourse/resources/views/about.blade.php
+++ LaravelFilamentBeginnerPraticalCourse/resources/views/about.blade.php
@@ -143,32 +143,32 @@
         <ul class="navbar-nav">
 
           <li class="nav-item">
-            <a class="nav-link " href="index">Home</a>
+            <a class="nav-link " href="/">Home</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="about.html">About</a>
+            <a class="nav-link" href="about">About</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link " href="property-grid.html">Property</a>
+            <a class="nav-link " href="property">Property</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link " href="blog-grid.html">Blog</a>
+            <a class="nav-link " href="blog">Blog</a>
           </li>
 
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
             <div class="dropdown-menu">
-              <a class="dropdown-item " href="property-single.html">Property Single</a>
-              <a class="dropdown-item " href="blog-single.html">Blog Single</a>
-              <a class="dropdown-item " href="agents-grid.html">Agents Grid</a>
-              <a class="dropdown-item " href="agent-single.html">Agent Single</a>
+              <a class="dropdown-item " href="property-single">Property Single</a>
+              <a class="dropdown-item " href="blog-single">Blog Single</a>
+              <a class="dropdown-item " href="agents-grid">Agents Grid</a>
+              <a class="dropdown-item " href="agent-single">Agent Single</a>
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link " href="contact.html">Contact</a>
+            <a class="nav-link " href="contact">Contact</a>
           </li>
         </ul>
       </div>
--- LaravelFilamentBeginnerPraticalCourse/routes/web.php
+++ LaravelFilamentBeginnerPraticalCourse/routes/web.php
@@ -3,7 +3,39 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('startpage');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/property', function () {
+    return view('property');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/agents-grid', function () {
+    return view('agents-grid');
+});
+
+Route::get('/agent-single', function () {
+    return view('agent-single');
+});
+
+Route::get('/blog-single', function () {
+    return view('blog-single');
+});
+
+Route::get('/property-single', function () {
+    return view('property-single');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
 
 Route::middleware([
