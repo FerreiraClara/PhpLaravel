3cac4395a2f7fb92571a390068d90632d8e4e1b2
Clara Rocha Ferreira <clararf.ferreira@gmail.com>
2025-05-06 11:48:10 -0300

    Buscando apenas as Temporadas de uma determinada serie

--- controle-serie/routes/api.php
+++ controle-serie/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SeriesController;
+use App\Http\Controllers\Api\TemporadasController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,5 @@ Route::apiResource('/series', SeriesController::class);
 // Route::get('/series', [SeriesController::class, 'index']);
 // Route::post('/series', [SeriesController::class, 'store']);
 // Route::get('/series/{id}', [SeriesController::class, 'showId']);
+
+Route::get('/series/{series}/season', [TemporadasController::class, 'index']);
